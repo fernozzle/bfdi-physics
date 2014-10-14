cbf37ca2fdf036860d9498263491471ac0f7f796
Michael Huang <fernozzle@htwins.net>
2014-10-14 10:41:44 -0700

    Make default `enumerable: false` more explicit

--- js/PropManager.js
+++ js/PropManager.js
@@ -2,7 +2,7 @@ var PropManager = {
 	managers: {},
 	init: function(prop, stage, phys2D, world) {
 		var manager = this._getManager(prop.name);
-		Object.defineProperty(prop, 'manager', {value: manager});
+		Object.defineProperty(prop, 'manager', {value: manager, enumerable: false});
 		manager.init(prop, stage, phys2D, world);
 	},
 	_getManager: function(name) {
